c3084b8eec665771b33fa8f78c6a88f5123b914d
Miguel Molina <statixr2@gmail.com>
2025-03-16 13:21:25 +0800

    Fixed expiration calculation

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,7 +6,7 @@ export const generateToken = (userID, res) =>{
     })
 
     res.cookie("jwt", token, {  // cookie comes from express
-        maxAge: 7*24*60*1000, // 7days in ms
+        maxAge: 7*24*60*60*1000, // 7days in ms
         httpOnly: true, //prevents xss attacks cross-site scripting attacks
         sameSite: "strict", // CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !== "development",
